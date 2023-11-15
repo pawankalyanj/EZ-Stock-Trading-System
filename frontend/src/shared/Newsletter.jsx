35ae6e5d1d74a3940ce4bd82dfba298ae0be7d94
Ganesh Nehru <54563238+ganeshnehru@users.noreply.github.com>
2023-11-15 13:20:30 -0800

    Modified UI

--- frontend/src/shared/Newsletter.jsx
+++ frontend/src/shared/Newsletter.jsx
@@ -1,33 +1,89 @@
-import React from 'react'
-import "./news-letter.css"
-import {Container,Row,Col} from "reactstrap"
-import newsletter from "../assets/images/newsletter.png"
-const Newsletter =()=>{
+// import React from "react";
+// import "./news-letter.css";
+// import { Container, Row, Col } from "reactstrap";
+// import newsletter from "../assets/images/newsletter.png";
 
-return <>
+// const Newsletter = () => {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // Handle form submission logic here
+//   };
 
-<section className='newsletter'>
-    <Container>
+//   return (
+//     <>
+//       <section className="newsletter">
+//         <Container>
+//           <Row>
+//             <Col lg="6">
+//               <div className="newsletter__content">
+//                 <h2> Subscribe now for valuable investment insights.</h2>
+//               </div>
+//               <div className="newsletter__input">
+//                 <input
+//                   type="email"
+//                   placeholder="Enter your email"
+//                   style={{ border: "2px solid black" }}
+//                 />
+//                 <button className="btn newsletter__btn btn btn-dark"> Subscribe </button>
+//               </div>
+//             </Col>
+//             <Col lg="6">
+//               <div className="newsletter__img">
+//                 <img src={newsletter} alt="" />
+//               </div>
+//             </Col>
+//           </Row>
+//         </Container>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Newsletter;
+
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import newsletterImage from "../assets/images/newsletter.png";
+import "./news-letter.css";
+
+const Newsletter = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <section className="newsletter">
+      <Container>
         <Row>
-            <Col lg='6'>
-                <div className='newsletter__content'>
-                    <h2> Subscribe now  to get useful investing information.</h2>
-                </div>
-                <div className='newsletter__input'>
-                    <input type="email" placeholder="Enter your email"/>
-                    <button className='btn newsletter__btn'> Subscribe </button>
-                </div>
-            </Col>
-            <Col lg="6">
-                <div className='newsletter__img'>
-                    <img src={newsletter} alt=""/>
-                </div>
-            </Col>
+          <Col lg="6">
+            <div className="newsletter__content">
+              <h2>Subscribe for Valuable Investment Insights</h2>
+              <p>Stay updated with the latest trends and market analyses.</p>
+            </div>
+            <div className="newsletter__input">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="newsletter__email-input"
+              />
+              <button
+                className="btn newsletter__btn btn btn-dark"
+                onClick={handleSubmit}
+              >
+                Subscribe
+              </button>
+            </div>
+          </Col>
+          <Col lg="6">
+            <div className="newsletter__img">
+              <img src={newsletterImage} alt="Newsletter" />
+            </div>
+          </Col>
         </Row>
-    </Container>
+      </Container>
     </section>
-</>
-
+  );
 };
 
 export default Newsletter;
