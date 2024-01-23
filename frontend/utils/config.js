9dc308c16d9dc2094a992b3635dd469c70d3db57
pawankalyanj <98665897+pawankalyanj@users.noreply.github.com>
2024-01-23 05:41:37 -0800

    Update config.js

--- frontend/utils/config.js
+++ frontend/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://52.53.91.202:4000";
+export const BASE_URL = "http://localhost:4000";
 export const PUBLIC_KEY = "pk_test_51M269VDeJb5NOKrEdyt4L7erYJcC1zZ5bj6xBLK7SnHRtk9YqC6jAGQVZKDWQU4FFKQtetEVQHI5pCiy2kk7jxxg00rxfCaGGV"
